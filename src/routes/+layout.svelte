144e322385368edfb07740603fea4e90eeb5b65d
Ben Cooper <cooperben83@gmail.com>
2024-03-12 19:34:20 -0400

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.css';
 	import Header from '../components/Header.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	import { page } from '$app/stores';
 
