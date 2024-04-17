420452176914c1f672ba85c871e4cd5b36181fbc
Ben Cooper <cooperben83@gmail.com>
2024-04-17 06:40:30 -0400

    fixed font size shifts and hover state

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -28,19 +28,21 @@
 		class="sticky top-0 inset-x-0 w-full bg-custom-black text-gray-100 font-display flex justify-between py-4 px-2 sm:p-6 border-b border-gray-500 z-50"
 		in:fade|global
 	>
-		<a href="/" class="hover:text-yellow-200">
-			<h3 class="text-lg sm:text-3xl">Ben Cooper</h3>
+		<a href="/" class="sm:hover:text-yellow-200">
+			<h3 class="text-2xl sm:text-3xl">Ben Cooper</h3>
 		</a>
-		<div class="text-sm md:text-lg flex gap-4 items-center">
-			<a href="/" class="hover:text-yellow-200" class:active={$page.url.pathname === '/'}
+		<div class="text-md md:text-lg flex gap-4 items-center">
+			<a href="/" class="sm:hover:text-yellow-200" class:active={$page.url.pathname === '/'}
 				>Portfolio</a
 			>
-			<a href="/about" class="hover:text-yellow-200" class:active={$page.url.pathname === '/about'}
-				>About</a
+			<a
+				href="/about"
+				class="sm:hover:text-yellow-200"
+				class:active={$page.url.pathname === '/about'}>About</a
 			>
 			<a
 				href="/contact"
-				class="hover:text-yellow-200"
+				class="sm:hover:text-yellow-200"
 				class:active={$page.url.pathname === '/contact'}
 			>
 				Contact
